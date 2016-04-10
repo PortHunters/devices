194a76b81b77538f6d127ccd27506bf5544c0fb7
Victor <gamehostplay@gmail.com>
2016-04-10 08:55:27 +0500

    fix

--- custom.bat
+++ custom.bat
@@ -9,6 +9,7 @@ for /d %%I in ("*.zip.bzprj") do (
 popd
 set ROM=%CD%\%RD%\baseROM
 set Tools=%CD%\data\tools
+set pht=%CD%\data\tools\ph-tools
 set Repo=%CD%\repositories\patches\main\boot
 
 del %Tools%\*.img
@@ -19,6 +20,8 @@ ren %ROM%\boot.img boot_PORT.img
 move /Y %ROM%\boot_PORT.img %Tools%\boot_PORT.img
 
 call %Tools%\MTK_unpack.bat boot_PORT.img 
+
+java -jar %pht%\ph-cr.jar
 
 copy /Y %Repo%\kernel\* %Tools%\boot_PORT\
 copy /Y %Repo%\rmdisk\* %Tools%\boot_PORT\rmdisk\
