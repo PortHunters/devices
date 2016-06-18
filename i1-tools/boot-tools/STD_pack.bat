44ddfcac84fdcba00ecdcf2135bcd88619eaadac
Victor <gamehostplay@gmail.com>
2016-06-18 17:38:26 +0500

    fix

--- i1-tools/boot-tools/STD_pack.bat
+++ i1-tools/boot-tools/STD_pack.bat
@@ -8,27 +8,48 @@ if (%1)==() (
 setlocal enabledelayedexpansion
 COLOR 0A
 mode con:cols=128 lines=50
+
+echo Boot_Recovery_Repack by michfood Jan 2015
+echo  MTK_pack.bat v4
+echo.
+
 set pt=%~N1%~X1
+echo Input folder - %1
+echo.
 cd %pt%
 %~dp0bin\chmod og=xr rmdisk
 cd rmdisk
+echo.
 echo - pack rmdisk to cpio...
 %~dp0bin\find . | %~dp0bin\cpio.exe -o -H newc -F ../new_ram_disk >nul
 move ..\ram_disk ..\ram_disk_old >nul
+move ..\new_ram_disk ..\ram_disk>nul
+echo.
 echo - pack rmdisk.cpio to gzip...
-%~dp0bin\gzip -f ../new_ram_disk
+%~dp0bin\gzip -n -f ../ram_disk
 
 rem set BASE="0x$(od -A n -h -j 34 -N 2 ./boot.img|sed 's/ //g')0000"
 rem set CMDLINE="$(od -A n --strings -j 64 -N 512 ./boot.img)"
-echo - make new image...
+echo.
+echo - make image file...
 FOR /F  %%i IN (../pagesize.txt) DO (set ps=%%i)
-echo - pagesize %ps%
-%~dp0bin\mkbootimg.exe --pagesize %ps% --kernel ../kernel --ramdisk ../new_ram_disk.gz -o ../new_image.img 
-copy ..\new_image.img %~dp0\new_image.img
+rem echo.
+rem echo - pagesize %ps%
+rem echo.
+%~dp0bin\mkbootimg.exe --pagesize %ps% --kernel ../kernel --ramdisk ../ram_disk.gz -o ../new_image.img 
+copy ..\new_image.img %~dp0\new_image.img  >nul 
 move ..\ram_disk_old ..\ram_disk >nul
 cd ..
 cd ..
-echo - done.
+echo.
+echo - Done.
+echo.
+if exist "new_image.img" (
+	echo - New image saved as new_image.img. Success.
+) else (
+	echo - New image did not created.     Fail.
+)
+echo.
 exit
 :end
 
--- i1-tools/boot-tools/STD_unpack.bat
+++ i1-tools/boot-tools/STD_unpack.bat
@@ -8,10 +8,18 @@ if (%1)==() (
 setlocal enabledelayedexpansion
 COLOR 0A
 mode con:cols=128 lines=50
+
+
+
 bin\sfk166.exe hexfind %1 -pat -bin /000000000000A0E10000A0E1/ -case >bin\offset.txt
 bin\sfk166.exe hexfind %1 -pat -bin /000000001F8B08/ -case >>bin\offset.txt 
 bin\sfk166.exe find bin\offset.txt -pat offset>bin\off2.txt
 bin\sfk166.exe replace bin\off2.txt -binary /20/0A/ -yes
+
+cls
+echo Boot_Recovery_Repack by michfood Jan 2015
+echo  STD_unpack.bat v4
+echo.
 
 if exist %~N1 rd /s /q %~N1 >nul
 
@@ -45,17 +53,19 @@ echo %ps%>%~N1\pagesize.txt
 echo - size of image   - %boot_size% byte
 echo - ram_disk offset - %real_ofs%
 echo.
-
 del bin\offset.txt
 del bin\off2.txt
 
-
+echo.
 echo - split kernel...
-bin\sfk166.exe partcopy %1 -fromto 0x1000 %real_ofs% %~N1\kernel -yes
+bin\sfk166.exe partcopy %1 -fromto %ps% %real_ofs% %~N1\kernel -yes
+echo.
 echo - extract ram_disk.gz...
 bin\sfk166.exe partcopy %1 -fromto %real_ofs% %boot_size% %~N1\ram_disk.gz -yes
+echo.
 echo - unpack ram_disk.gz...
 bin\7z.exe -tgzip x -y %~N1\ram_disk.gz -o%~N1 >nul
+echo.
 echo - unpack ram_disk.cpio...
 md %~N1\rmdisk
 cd %~N1
@@ -63,7 +73,26 @@ cd rmdisk
 %~dp0bin\cpio.exe -i <../ram_disk
 cd ..
 cd ..
-echo - copy source %1 to unpacked folder ^(to keep source image^)...
-echo - done.
+echo.
+copy %1 %~N1  >nul 
+echo.
+echo - Done.
+echo.
+if exist "%~N1/kernel" (
+	echo - %~N1/kernel exist.        Success.
+) else (
+	echo - %~N1/kernel do not exist. Fail.
+)
+if exist "%~N1/rmdisk" (
+	echo - %~N1/rmdisk exist.        Success.
+) else (
+	echo - %~N1/rmdisk do not exist. Fail.
+)
+if exist "%~N1/rmdisk/*" (
+	echo - %~N1/rmdisk is not empty. Success.
+) else (
+	echo - %~N1/rmdisk is empty.     Fail.
+)
+echo.
 exit
 :end
