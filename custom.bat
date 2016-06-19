8d09a1f4c957544d520688854450895c912b6234
Victor <gamehostplay@gmail.com>
2016-06-19 19:28:47 +0500

    fix

--- custom.bat
+++ custom.bat
@@ -1,36 +1,14 @@
 @echo off
 chcp 1251 >nul
 
+echo custom.bat by Victor Malyshev (@I1PABIJJA) i1pabijja@gmail.com
+
 set CD=%~dp0
-pushd "%~dp0"
-for /d %%I in ("*.zip.bzprj") do (
-    set RD="%%~I"
-)
-popd
-set ROM=%CD%\%RD%\baseROM
-set Tools=%CD%\data\tools
-set pht=%CD%\data\tools\ph-tools
-set Repo=%CD%\repositories\patches\main\boot
+for /d %%I in ("*.zip.bzprj") do (set Project=%%~I)
+set Rom=%CD%\%Project%\baseROM\
+set Tools=%CD%\i1-tools\
+set Repo=%CD%\repositories\
 
-del %Tools%\*.img
-rmdir /S /Q %Tools%\boot_PORT
+rem standart Args: -pdir %Repo%\patches\main\other\ -uarg:boot %Tools%\boot-tools\ -uarg:repo %Repo% -uarg:rom %Rom%
 
-ren %ROM%\boot.img boot_PORT.img
-
-move /Y %ROM%\boot_PORT.img %Tools%\boot_PORT.img
-
-call %Tools%\MTK_unpack.bat boot_PORT.img 
-
-rem java -jar %pht%\ph-cr.jar
-
-copy /Y %Repo%\kernel\* %Tools%\boot_PORT\
-copy /Y %Repo%\rmdisk\* %Tools%\boot_PORT\rmdisk\
-
-call %Tools%\MTK_pack.bat boot_PORT
-
-move /Y %Tools%\new_image.img %ROM%\boot.img
-
-del %Tools%\*.img
-rmdir /S /Q %Tools%\boot_PORT
-
-echo Repacking Boot.img finished...
+java -jar %Tools%\i1atcher.jar -pdir %Repo%\patches\main\other\ -uarg:boot %Tools%\boot-tools\ -uarg:repo %Repo% -uarg:rom %Rom%
